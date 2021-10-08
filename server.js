fb06d646b08ffb588b68120ea7472969470812ac
zahidhasan9 <44033232+zahidhasan9@users.noreply.github.com>
2021-10-08 16:50:48 +0600

    add all

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const env = require("dotenv");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+
+// Imoprt
+const connectDB = require('./config/db');
+
+
+//routes
+const authRoutes = require("./src/routes/auth");
+const adminRoutes = require("./src/routes/admin/auth");
+
+// Config dotenv
+require('dotenv').config({
+    path: './config/.env'
+});
+
+
+
+//environment variable or you can say constants
+env.config();
+
+// mongodb connection
+
+connectDB();
+
+
+app.use(cors());
+app.use(express.json());
+// app.use("/public", express.static(path.join(__dirname, "uploads")));
+app.use("/api", authRoutes);
+app.use("/api", adminRoutes);
+// app.use("/api", categoryRoutes);
+// app.use("/api", productRoutes);
+// app.use("/api", cartRoutes);
+// app.use("/api", initialDataRoutes);
+// app.use("/api", pageRoutes);
+// app.use("/api", addressRoutes);
+// app.use("/api", orderRoutes);
+// app.use("/api", adminOrderRoute);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
